82449b51de7bd3813b1aaa69f769f7645fd7d79e
Elena Rotari <elena.rotari@icloud.com>
2023-02-12 21:59:10 +0100

    styled our production

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import "./Product.scss";
 import { useOutletContext } from "react-router-dom";
+import { BiRightArrowAlt, BiLeftArrowAlt } from "react-icons/bi";
 
 function Products() {
   const data = useOutletContext();
-  console.log(data);
-  return (
-    <div>
-      <h1>Our Products</h1>
 
-      <div>
+  return (
+    <div className="containerProduct">
+      <div className="sales">
+        <div className="textSales">
+          <p>SALES%</p>
+          <p>Special Week Offer</p>
+        </div>
+        <div className="imgSales">
+          {/* {data.data && <img src={data?.data[2].picture} />} */}
+        </div>
+      </div>
+      <h1>Our Products</h1>
+      <div className="products">
         {data?.data.map((el, _id) => (
-          <div key={el._id}>
+          <div key={el._id} className="cardProducts">
             <img src={el.picture} />
-            <p>{el.title}</p>
-            <p>{el.artist}</p>
-            <p>{el.price}</p>
-            <p>{el.year}</p>
+            <h2>Title: {el.title}</h2>
+            <h3> Artist: {el.artist}</h3>
+            <h4> Year: {el.year}</h4>
           </div>
         ))}
       </div>
